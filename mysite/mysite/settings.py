0f3541f0e76fcc5f530fc70df8a7a6353b2e73a9
interlochen <rcd53@nau.edu>
2015-09-11 12:07:27 -0400

    site working

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,16 +104,18 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/webapps/norway/static/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_HOST_USER = 'interlochen'
-# EMAIL_HOST_PASSWORD = '1stinkykitty'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'norwaylrexpedition@gmail.com'
+EMAIL_HOST_PASSWORD = 'seam2011'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- mysite/overland/views.py
+++ mysite/overland/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail, BadHeaderError
 from django.template import RequestContext
+from django.conf import settings
 
 from .models import Application
 from .models import ApplyForm
@@ -25,14 +26,14 @@ def application(request):
 				year = form.cleaned_data['year']
 				make = form.cleaned_data['make']
 				model = form.cleaned_data['model']
-				message = str(names) + '\n' + str(phone) + '\n' + str(year) + '\n' + str(make) + '\n' + str(model)
+				message = str(names) + '\n' + str(from_email) + '\n' + str(phone) + '\n' + str(year) + '\n' + str(make) + '\n' + str(model)
 				try:
-					send_mail(subject, message, from_email, ['rcd53@nau.edu'])
+					send_mail(subject, message, settings.EMAIL_HOST_USER, ['norwaylrexpedition@gmail.com'], fail_silently=False)
 				except BadHeaderError:
 					return HttpResponse('Invalid header found.')
 				return redirect('thanks')
-			except:
-				pass
+			except Exception as e:
+				print(e)
 	return render(request, "overland/apply.html", {'form': form})
 
 def route(request):
