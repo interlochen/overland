12db8f6ba7d16b4eaf67ed8e819296fc272da6ed
interlochen <rcd53@nau.edu>
2015-11-15 17:51:19 -0700

    asdfa

--- mysite/static/js/init.js
+++ mysite/static/js/init.js
@@ -58,13 +58,30 @@ skel.init({
 });
 
 skel.breakpoints({
-    medium: "(min-width: 769px) and (max-width: 1024px)",
-    small:  "(max-width: 768px)",
+    skel.breakpoints({
+    xlarge: "(max-width: 1680px)",
+    large:  "(max-width: 1280px)",
+    medium: "(max-width: 980px)",
+    small:  "(max-width: 736px)",
     xsmall: "(max-width: 480px)"
 });
 
 skel.layout({
+    skel.layout({
     reset: "normalize",
+    containers: true,
     grid: true,
-    containers: true
+    breakpoints: {
+        medium: {
+            containers: "90%"
+        },
+        small: {
+            containers: "95%",
+            grid: { gutters: 20 }
+        },
+        xsmall: {
+            grid: { gutters: 10 }
+        }
+    }
+});
 });
