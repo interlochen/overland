35972b38470106bf682528830128a56f5087665d
interlochen <rcd53@nau.edu>
2016-01-02 12:18:00 -0700

    change settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vn*l1pwv4*rr78)ejwt9x64b2nsgcvosk@q9#-a=%xnzexj*ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
